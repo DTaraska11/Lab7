e3c903a0735970ccdc488523a8eec5f9728036ae
Dominic Taraska <tug76525@temple.edu>
2020-11-11 00:41:11 -0500

    Lab8 other files

---
+++ app/src/main/java/temple/edu/BrowserControlFragment.java
@@ -0,0 +1,94 @@
+package temple.edu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BrowserControlFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BrowserControlFragment extends Fragment implements View.OnClickListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    ImageButton imageButton;
+    BrowserControlListener callback;
+    public void setBrowserControlListener(BrowserControlListener callback){
+        this.callback = callback;
+    }
+    public BrowserControlFragment() {
+        // Required empty public constructor
+    }
+    public interface BrowserControlListener{
+        public void fragmentAdded(String url);
+    }
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BrowserControlFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BrowserControlFragment newInstance(String param1, String param2) {
+        BrowserControlFragment fragment = new BrowserControlFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_browser_control, container, false);
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(this);
+
+
+        return view;
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageButton:
+                callback.fragmentAdded("n");
+                break;
+            case R.id.imageButton2:
+
+                break;
+            // Do this for all buttons.
+        }
+    }
+}
---
+++ app/src/main/java/temple/edu/PageListFragment.java
@@ -0,0 +1,108 @@
+package temple.edu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PageListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PageListFragment extends Fragment implements View.OnClickListener  {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    ListView listView;
+
+    ArrayAdapter<TextView> adapter;
+    OnTitleSelectedListener callback;
+    public void setOnTitleSelectedListener(OnTitleSelectedListener callback){
+        this.callback = callback;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public interface OnTitleSelectedListener{
+        public void OnTitleSelected(int index);
+    }
+    public PageListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PageListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PageListFragment newInstance(String param1, String param2) {
+        PageListFragment fragment = new PageListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_page_list, container, false);
+
+
+        listView = view.findViewById(R.id.list);
+        adapter = new ArrayAdapter<TextView>(this.getContext(), R.layout.simplerow);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView item = (TextView) parent.getItemAtPosition(position);
+                int index = parent.getSelectedItemPosition();
+                callback.OnTitleSelected(position);
+            }
+        });
+
+        return view;
+
+    }
+
+    public void refreshList(ArrayAdapter<TextView> adapter){
+        this.adapter = adapter;
+        this.listView.setAdapter(adapter);
+    }
+}
